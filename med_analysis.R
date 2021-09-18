507a1114ba6d1d3f4f664229273dae44014b263a
nmonhait <nichole.monhait@gmail.com>
2021-09-18 16:44:53 +0000

    date diff

--- med_analysis.R
+++ med_analysis.R
@@ -21,11 +21,12 @@ medications <- medications %>%
            payer_coverage, reason_code, reason)
 
 #format date
-medications %>% 
+medications <- medications %>% 
     mutate(start = ymd_hms(start),
            stop = ymd_hms(stop),
            start = floor_date(start, unit = "day"),
            stop = floor_date(stop, unit = "day"),
            start = date(start),
            stop = date(stop),
+           care_time = difftime(stop, start, units = "days")
            )
