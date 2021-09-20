1d861f3bb0839345b0f5c5cd6e98e9620a89d20c
nmonhait <nichole.monhait@gmail.com>
2021-09-20 13:44:28 +0000

    analysis 9/20

--- med_analysis.R
+++ med_analysis.R
@@ -1,7 +1,12 @@
 #analysis of medication df
 
 #libraries
+library(tidyverse)
+library(readr)
 library(lubridate)
+
+#import data
+medications <- read_csv("data/medications.csv")
 
 #clean data
 medications <- medications %>% 
@@ -32,9 +37,53 @@ medications <- medications %>%
            )
 
 #reformat care_time variable 
-medication_by_length <- medications %>% 
+medications <- medications %>% 
     separate(care_time, into = c("care_time_days", NA), sep = " ") %>% 
     select(code, description, base_cost, payer_coverage, reason_code,
-           reason, care_time_days)
-View(medication_by_length
-     )
+           reason, care_time_days) %>% 
+    mutate(care_time_days = as.numeric(care_time_days)) %>% 
+    mutate(out_of_pocket_cost = (base_cost - payer_coverage))
+
+
+#most diagnosed reason
+top_reasons_for_med <- medications %>% 
+    group_by(reason) %>%
+    summarize(reason_count = n()) %>% 
+    arrange(desc(reason_count)) %>% 
+    ungroup()
+
+#most diagnosed drug
+top_med <- medications %>% 
+    group_by(description) %>%
+    summarize(prescription_count = n()) %>% 
+    arrange(desc(prescription_count)) %>% 
+    ungroup()
+
+
+#avg care time for diagnosis
+care_time_by_reason <- medications %>% 
+    group_by(reason) %>% 
+    summarize(avg_care_time = mean(care_time_days)) %>% 
+    arrange(desc(avg_care_time)) %>% 
+    ungroup() 
+
+#cost by reason
+cost_by_reason <- medications %>% 
+    group_by(reason) %>% 
+    summarize(avg_cost_reason = mean(out_of_pocket_cost, na.rm = TRUE)) %>% 
+    arrange(desc(avg_cost_reason)) %>% 
+    ungroup()
+              
+#cost by drug  
+cost_by_drug <- medications %>% 
+    group_by(description) %>% 
+    summarize(avg_cost_reason = mean(out_of_pocket_cost, na.rm = TRUE)) %>% 
+    arrange(desc(avg_cost_reason)) %>% 
+    ungroup()
+
+#plot cost by reason
+cost_by_reason %>% 
+    ggplot() +
+    geom_col(aes(x = reason, y = avg_cost_reason))
+          
+    
